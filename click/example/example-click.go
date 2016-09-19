311394e545c7951c7ea2486dba66a6d315acc507
amortaza <davidnia1@gmail.com>
2016-09-18 22:33:31 -0700

    click example

---
+++ click/example/example-click.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"runtime"
+	"fmt"
+	"github.com/amortaza/go-bellina"
+	"github.com/amortaza/go-dark-ux"
+	"github.com/amortaza/go-dark-ux/border"
+	"github.com/amortaza/go-bellina-plugins/click"
+	"github.com/amortaza/go-hal-g5"
+)
+
+func initialize() {
+	go_dark_ux.Init()
+}
+
+func tick() {
+
+	bl.Root()
+	{
+		bl.Pos(0,0)
+		bl.Dim(1024, 768)
+		border.Fill(50, 0, 0)
+
+		bl.Div()
+		{
+			bl.Id("green")
+			bl.Pos(10,10)
+			bl.Dim(100,100)
+
+			border.Fill(0, 50, 0)
+
+			click.On(func(e interface{}) {
+				clickEvent := e.(click.Event)
+
+				fmt.Println("Clicked on", clickEvent.Target.Id)
+				fmt.Println("Clicked at", clickEvent.X, clickEvent.Y)
+			})
+		}
+		bl.End()
+
+		bl.Div()
+		{
+			bl.Id("blue")
+			bl.Pos(110, 110)
+			bl.Dim(100, 100)
+
+			border.Fill(0, 0, 70)
+
+			click.On_WithLifeCycle(
+
+				// click success!
+				func(e interface{}) {
+					clickEvent := e.(click.Event)
+
+					fmt.Println("Clicked on", clickEvent.Target.Id, clickEvent.X, clickEvent.Y)
+				},
+
+				// click initiated
+				func(e interface{}) {
+					clickEvent := e.(click.Event)
+
+					fmt.Println("Mouse Down", clickEvent.Target.Id, clickEvent.X, clickEvent.Y)
+				},
+
+				// click failed
+				func(e interface{}) {
+					// e will be nil
+
+					fmt.Println("Click miss")
+				},
+			)
+		}
+		bl.End()
+	}
+	bl.End()
+}
+
+func uninit() {
+}
+
+func init() {
+	runtime.LockOSThread()
+}
+
+func main() {
+	bl.Start( hal_g5.New(), 1280, 1024, "Bellina v0.9", initialize, tick, uninit )
+
+	fmt.Println("bye!")
+}
+
+
